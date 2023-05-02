688813b84abfc8a93385ce9d78016f68e73c18fd
Maisha Khan <maishasaiyara@gmail.com>
2023-05-02 13:18:06 -0500

    third commit

--- db/routines.js
+++ db/routines.js
@@ -1,4 +1,5 @@
 const client = require("./client");
+const { attachActivitiesToRoutines } = require("./activities");
 
 async function createRoutine({ creatorId, isPublic, name, goal }) {
   try {
@@ -6,9 +7,10 @@ async function createRoutine({ creatorId, isPublic, name, goal }) {
       INSERT INTO routines("creatorId", "isPublic", name, goal)
       VALUES ($1, $2, $3, $4)
       ON CONFLICT (name) DO NOTHING
-      RETURNING *;`
-      , [creatorId, isPublic, name, goal])
-      return routine;
+      RETURNING *
+      `,[creatorId, isPublic, name, goal])
+    
+    return routine;
   } catch (error) {
     throw error;
   }
@@ -39,11 +41,20 @@ async function getRoutinesWithoutActivities() {
 
 async function getAllRoutines() {
   try {
-    
-  } catch (error) {
-    
-  }
+    const { rows: routines } = await client.query(
+          `
+          SELECT routines.*, users.username AS "creatorName" 
+          FROM routines
+          JOIN users ON routines.creatorId = users.id
+          `);
+
+          return await attachActivitiesToRoutines(routines);
+    }catch (error) {
+      throw error;
+    }          
 }
+
+// Join routine activities
 
 async function getAllPublicRoutines() {
   try {
@@ -57,7 +68,11 @@ async function getAllPublicRoutines() {
   }
 }
 
-async function getAllRoutinesByUser({ username }) {}
+// Why is there no test?
+
+async function getAllRoutinesByUser({ username }) {
+
+}
 
 async function getPublicRoutinesByUser({ username }) {}
 
